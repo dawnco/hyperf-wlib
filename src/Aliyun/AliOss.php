4cbda93d5b83f619f17d879e675efe2f6e9cb7db
Daw <abke@qq.com>
2022-06-23 21:23:01 +0800

    f

--- src/Aliyun/AliOss.php
+++ src/Aliyun/AliOss.php
@@ -124,4 +124,88 @@ class AliOss extends Aliyun
         }
 
     }
+
+
+    /**
+     * 获取携带签名url
+     * @param string $filename 路径 /image/1.jpg
+     * @param string $bucket   file-storage
+     * @param int    $expires  到期的时间戳
+     * @return string[]
+     * @throws AppException
+     */
+    public function getUrlAuthorization(string $filename, int $expires, string $bucket = ''): array
+    {
+        if (!$bucket) {
+            $bucket = $this->config['bucket'];
+        }
+        if (!$bucket) {
+            throw new AppException('没指定 OSS bucket');
+        }
+        $header = [
+            'Host' => $bucket . '.' . $this->config['endpoint'],
+            'Content-MD5' => '',
+            'Content-Type' => '',
+        ];
+
+        $canonicalizeResource = '/' . $bucket . '/' . $filename;
+
+        $signature = $this->urlAuthorization(
+            $header,
+            'OSS',
+            'GET',
+            $filename,
+            $expires,
+            $canonicalizeResource,
+            ['x-oss']
+        );
+
+        $url = "https://" . $bucket . '.' . $this->config['endpoint'] . '/' . $filename;
+
+        $responseUri = $url . '?' .
+                       'OSSAccessKeyId=' . $this->config['accessId'] .
+                       '&Expires=' . $expires .
+                       '&Signature=' . $signature;
+
+        return [
+            'url' => $responseUri
+        ];
+    }
+
+
+    protected function urlAuthorization(
+        array $header,
+        string $type,
+        string $method,
+        string $filename,
+        int $expires,
+        string $canonicalizeResource,
+        array $canonicalizeHeaders = []
+    ): ?string {
+
+        if (isset($header['Date'])) {
+            unset($header['Date']);
+        }
+
+        if ($expires < time()) {
+            throw new AppException('Expiration time cannot be less than current time');
+        }
+
+        if ($method == 'GET') {
+            $header['Content-MD5'] = '';
+            $header['Content-Type'] = '';
+        }
+
+        $canonicalizeHeaders = $this->canonicalizeHeaders($header, $canonicalizeHeaders);
+
+        $str = $method . "\n";
+        $str .= ($header['Content-MD5'] ?? '') . "\n";
+        $str .= ($header['Content-Type'] ?? '') . "\n";
+        $str .= $expires . "\n";
+        $str .= $canonicalizeHeaders;
+        $str .= $canonicalizeResource;
+
+        return urlencode(base64_encode(hash_hmac('sha1', $str, $this->config['secretKey'], true)));
+    }
+
 }
