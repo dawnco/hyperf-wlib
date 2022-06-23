086f973c4966e4e5ac775581602857be7c917058
Daw <abke@qq.com>
2022-06-23 14:44:40 +0800

    fx

--- src/WTable.php
+++ src/WTable.php
@@ -9,15 +9,23 @@ declare(strict_types=1);
 
 namespace WLib;
 
+use App\Exception\AppException;
+use WLib\Constant\CountryConstant;
+
 class WTable
 {
     /**
      * 日期名称
      * @param int $timestamp
      * @return string
+     * @throws AppException
      */
     public static function name(int $timestamp): string
     {
-        return WDate::getInstance('cn')->setTimestamp($timestamp)->format("ym");
+        $country = WConfig::get('app_country');
+        if (!$country) {
+            throw new AppException("app_country config not set");
+        }
+        return WDate::getInstance(CountryConstant::toId($country))->setTimestamp($timestamp)->format("ym");
     }
 }
