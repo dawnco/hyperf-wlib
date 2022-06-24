dd485a265613abf2e10df1e27dec2bf2a8bb32f7
Daw <abke@qq.com>
2022-06-24 23:39:49 +0800

    fx

---
+++ src/WRpc.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * @author Dawnc
+ * @date   2022-06-24
+ */
+
+namespace WLib;
+
+use App\Exception\AppException;
+
+class WRpc
+{
+    /**
+     * @throws AppException
+     * @throws \Throwable
+     */
+    public static function call(callable $call, string $rpcName)
+    {
+        try {
+            return $call();
+        } catch (\Hyperf\RpcClient\Exception\RequestException $e) {
+            $error = "$rpcName Rpc 请求异常 " . $e->getThrowableMessage();
+            WLog::error($error);
+            throw new AppException($error, $e->getThrowableCode() ?: -110, $e);
+        }
+    }
+}
