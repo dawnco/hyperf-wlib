f1e32b227ec570a2fd5ba6e8a6d4c1576c92de2f
Daw <abke@qq.com>
2025-03-27 10:32:06 +0800

    add center tool

---
+++ src/Lib/Center/EventCenter.php
@@ -0,0 +1,82 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * EC客户端
+ * @date   2025-03-26
+ */
+
+namespace WLib\Lib\Center;
+
+use WLib\WConfig;
+use WLib\WLog;
+
+class EventCenter
+{
+
+    private static $stream = null;
+
+    /**
+     * 发送事件数据
+     * @param string $name      事件名称
+     * @param array  $params    key的类型必须是字符串, value 类型任意 格式 ["key1"=>1, "key2"=>"value2"]
+     * @param string $requestId
+     * @param int    $timestamp 毫秒时间戳 默认 当前毫秒
+     * @param string $topic     指定写入那个kafka的
+     * @param string $from      来自那个服务 默认取配置的  app_name
+     * @return void
+     */
+    public static function app(
+        string $name,
+        array $params = [],
+        string $requestId = "",
+        int $timestamp = 0,
+        string $topic = "",
+        string $from = ""
+    ): void {
+        self::send(json_encode([
+            "name" => $name,
+            "_topic_" => $topic,
+            "requestId" => $requestId,
+            "from" => strval($from ?: WConfig::get("app_name")),
+            "country" => strval(WConfig::get("app_country") ?: ""),
+            "timestamp" => $timestamp ?: intval(microtime(true) * 1000),
+            "params" => $params ?: null
+        ], JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES));
+    }
+
+    protected static function send(string $data): void
+    {
+
+        if (self::$stream == null) {
+            self::$stream = stream_socket_client("udp://center.stat.com:9820", $errno, $error);
+            if (!self::$stream) {
+                return;
+            }
+            // https://www.php.net/manual/zh/function.stream-set-blocking.php
+            stream_set_blocking(self::$stream, false);
+        }
+        $str = self::pack($data);
+        if ($str && self::$stream) {
+            fwrite(self::$stream, $str);
+        }
+    }
+
+    protected static function pack(string $data): string
+    {
+        $length = strlen($data);
+        $type = pack('n', 55);
+        $remain = pack('C', 1);
+        $size = pack('n', $length);
+
+        if ($length > 10235) {
+            WLog::error("EventCenter 数据过长", ["data" => $data]);
+            return "";
+        }
+
+        return $type . $remain . $size . $data;
+    }
+
+
+}
---
+++ src/Lib/Center/LogCenter.php
@@ -0,0 +1,79 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * 日志客户端
+ * @date   2025-03-26
+ */
+
+namespace WLib\Lib\Center;
+
+use WLib\WLog;
+
+class LogCenter
+{
+
+    private static $stream = null;
+
+    /**
+     * 记录到sls
+     * @param string $store     那个store
+     * @param array  $data      key和value的类型必须是字符串 格式 ["key"=>"value", "name"=>"jard", "age"=>"25"]
+     * @param int    $timestamp 秒时间戳 默认当前秒
+     * @return void
+     */
+    public static function record(
+        string $store,
+        array $data,
+        int $timestamp = 0
+    ): void {
+
+        $kv = [];
+        foreach ($data as $k => $v) {
+            $kv[] = [
+                "key" => $k,
+                "val" => $v,
+            ];
+        }
+        self::send(json_encode([
+            "t" => $timestamp ?: time(), // 时间戳 秒
+            "store" => $store, // 那个store
+            "kv" => $kv
+        ], JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES));
+    }
+
+    protected static function send(string $data): void
+    {
+
+        if (self::$stream == null) {
+            self::$stream = stream_socket_client("udp://log.stat.com:8844", $errno, $error);
+            if (!self::$stream) {
+                return;
+            }
+            // https://www.php.net/manual/zh/function.stream-set-blocking.php
+            stream_set_blocking(self::$stream, false);
+        }
+        $str = self::pack($data);
+        if ($str && self::$stream) {
+            fwrite(self::$stream, $str);
+        }
+    }
+
+    protected static function pack(string $data): string
+    {
+        $length = strlen($data);
+        $type = pack('n', 60);
+        $remain = pack('C', 1);
+        $size = pack('N', $length);
+
+        if ($length > 10235) {
+            WLog::error("LogCenter 数据过长", ["data" => $data]);
+            return "";
+        }
+
+        return $type . $remain . $size . $data;
+    }
+
+
+}
